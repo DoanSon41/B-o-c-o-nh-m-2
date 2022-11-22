1bbd5e18c323b19ff5a4ebd4861ad8f46e05de1d
Đoàn Ngọc Sơn <beatergamming@gmail.com>
2022-11-22 21:16:52 +0700

    update

---
+++ Lab 3/Lab4C1-C4.sql
@@ -0,0 +1,110 @@
+﻿/câu 1/
+---------------------------------------------------------
+select da.TENDEAN, CAST(sum(pc.THOIGIAN) as decimal(8, 2)) as tong_so_gio_lam_viec 
+from DEAN da
+inner join phancong pc 
+on da.MADA = pc.MADA
+group by da.TENDEAN	
+-----------------------------------------------------------
+select da.TENDEAN, CAST(sum(pc.THOIGIAN) as varchar) as tong_so_gio_lam_viec 
+from DEAN da
+inner join phancong pc 
+on da.MADA = pc.MADA
+group by da.TENDEAN
+------------------------------------------------------------
+select pb.TENPHG, 
+REPLACE(CONVERT(nvarchar, CAST(avg(nv.LUONG) as decimal(10, 2))), '.', ',') 
+as luong_trung_binh 
+from PHONGBAN pb
+inner join NHANVIEN nv 
+on pb.MAPHG = nv.PHG
+group by pb.TENPHG
+---------------------------------------------------------
+select pb.TENPHG,
+left(CAST(avg(nv.LUONG) as varchar), 3) + 
+REPLACE(CAST(avg(nv.LUONG) as varchar), left(CAST(avg(nv.LUONG) as varchar), 3), ',')
+as luong_trung_binh
+from PHONGBAN pb
+inner join NHANVIEN nv 
+on pb.MAPHG = nv.PHG
+group by pb.TENPHG
+
+/câu 2/
+-------------------------------------------------------
+select da.TENDEAN, CEILING(CAST(sum(pc.THOIGIAN) as decimal(8, 2))) as tong_so_gio_lam_viec 
+from DEAN da
+inner join phancong pc 
+on da.MADA = pc.MADA
+group by da.TENDEAN
+------------------------------------------------------
+select da.TENDEAN, FLOOR(CAST(sum(pc.THOIGIAN) as decimal(8, 2))) as tong_so_gio_lam_viec 
+from DEAN da
+inner join phancong pc 
+on da.MADA = pc.MADA
+group by da.TENDEAN
+-------------------------------------------------------------
+select da.TENDEAN, CAST(sum(pc.THOIGIAN) as decimal(8, 2)) as tong_so_gio_lam_viec 
+from DEAN da
+inner join phancong pc 
+on da.MADA = pc.MADA
+group by da.TENDEAN
+----------------------------------------------------------------
+DECLARE @luong_trung_binh int
+SET @luong_trung_binh = (select avg(nv.LUONG)
+	as luong_trung_binh
+	from PHONGBAN pb
+	inner join NHANVIEN nv 
+	on pb.MAPHG = nv.PHG)
+select HONV, TENLOT, TENNV, LUONG from NHANVIEN 
+where NHANVIEN.LUONG > @luong_trung_binh
+
+/câu 3/
+-----------------------------------------------------------------
+select UPPER(nv.HONV) + ' ' + nv.TENLOT + ' ' + nv.TENNV as hoten, nv.DCHI, count(nt.TENTN) from NHANVIEN nv 
+inner join THANNHAN nt on nv.MANV = nt.MA_NVIEN
+group by nv.HONV, nv.TENLOT, nv.TENNV, nv.DCHI
+having count(nt.TENTN) > 2
+---------------------------------------------------------------------------------------
+select nv.HONV + ' ' + UPPER(nv.TENLOT) + ' ' + nv.TENNV as hoten, nv.DCHI, count(nt.TENTN) from NHANVIEN nv 
+inner join THANNHAN nt on nv.MANV = nt.MA_NVIEN
+group by nv.HONV, nv.TENLOT, nv.TENNV, nv.DCHI
+having count(nt.TENTN) > 2
+--------------------------------------------------------------------
+
+select nv.HONV + ' ' + nv.TENLOT + ' ' + 
+left(nv.TENNV, 1) +
+UPPER(right(left(nv.TENNV, 2), 1)) +
+right(nv.TENNV, LEN(nv.TENNV) - 2)
+as hoten, nv.DCHI, 
+nv.TENNV,
+count(nt.TENTN) as sonhanthan from NHANVIEN nv 
+inner join THANNHAN nt on nv.MANV = nt.MA_NVIEN
+group by nv.HONV, nv.TENLOT, nv.TENNV, nv.DCHI
+having count(nt.TENTN) > 2
+--------------------------------------------------------------
+select nv.HONV + ' ' + nv.TENLOT + ' ' + nv.TENNV as hoten,  
+SUBSTRING(nv.DCHI, PATINDEX('% %',nv.DCHI), PATINDEX('%,%',nv.DCHI) - PATINDEX('% %',nv.DCHI))
+as tenduong, 
+count(nt.TENTN) as sonhanthan from NHANVIEN nv 
+inner join THANNHAN nt on nv.MANV = nt.MA_NVIEN
+group by nv.HONV, nv.TENLOT, nv.TENNV, nv.DCHI
+having count(nt.TENTN) > 2
+
+/câu 4/
+----------------------------------------------------
+go
+Select *
+From NHANVIEN
+Where Year(NGSINH) Between 1960 and 1965
+go
+
+-----------------------------------------------------
+go	
+		select TENNV,Year(getdate())-Year(NGSINH) as 'Tuổi'
+		from NHANVIEN
+	go
+----------------------------------------------------------
+ SELECT TENPHG, CONVERT(VARCHAR, NG_NHANCHUC,105) AS 'NGAY SINH',HONV,TENLOT,TENNV,COUNT(*) NHANVIEN
+  FROM PHONGBAN, NHANVIEN
+  WHERE   MANV=TRPHG and MAPHG=PHG
+  GROUP BY TENPHG,NG_NHANCHUC,HONV,TENLOT,TENNV
---
+++ laB4/Lab4B1-B3.sql
@@ -0,0 +1,72 @@
+﻿SELECT IIF(LUONG>=LTB,'KHÔNG TĂNG LƯƠNG','Tăng lương')
+as thuong,tennv,luong,ltb
+from
+
+(select TENNV,LUONG,ltb from NHANVIEN,
+(select avg(LUONG) as 'ltb',PHG from NHANVIEN group by PHG) AS TAM
+WHERE NHANVIEN.PHG = TAM.PHG) as abc
+
+SELECT * FROM NHANVIEN
+select avg(LUONG) as 'ltb',PHG from NHANVIEN group by PHG
+
+
++++++++++++++++++++++++++++++++++++++++++++++++++++++
+SELECT IIF(LUONG>=LTB,'truongphong','nhanvien')
+as xeploai,tennv,luong,ltb
+from
+
+(select TENNV,LUONG,ltb from NHANVIEN,
+(select avg(LUONG) as 'ltb',PHG from NHANVIEN group by PHG) AS TAM
+WHERE NHANVIEN.PHG = TAM.PHG) as abc
+
+SELECT * FROM NHANVIEN
+select avg(LUONG) as 'ltb',PHG from NHANVIEN group by PHG
+
++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+select tennv = case PHAI
+WHEN 'NAM' THEN 'MR.'+[TENNV]
+WHEN N'Nữ' THEN 'MRS.'+[TENNV]
+END
+FROM NHANVIEN
+
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+SELECT TENNV,LUONG,THUE=CASE
+WHEN LUONG BETWEEN 0 AND 25000 THEN LUONG*0.1
+WHEN LUONG BETWEEN 25000 AND 30000 THEN LUONG*0.12
+WHEN LUONG BETWEEN 30000 AND 40000 THEN LUONG*0.15
+WHEN LUONG BETWEEN 40000 AND 50000 THEN LUONG*0.2
+ELSE LUONG*0.25 END
+FROM NHANVIEN
+
++++++++++++++++++++++++++++++++++++++++++++++++++++
+select * from NHANVIEN;
+
+DECLARE @MAX INT, @NUM INT;
+SELECT @MAX = MAX(CAST(MANV AS INT)) FROM NHANVIEN;
+
+SET @NUM =1;
+
+WHILE @NUM <= @MAX
+BEGIN
+	IF @NUM=4
+	BEGIN
+		SET @NUM=@NUM+1;
+		CONTINUE;
+	END
+
+	IF @NUM %2 =0
+	SELECT HONV,TENLOT, TENNV from NHANVIEN where cast (MANV as int) = @num;
+
+	SET @NUM = @NUM +1;
+END;
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+select * from PHONGBAN
+BEGIN TRY
+	insert into PHONGBAN(TENPHG,MAPHG,TRPHG, NG_NHANCHUC)
+	values (N'Sản Xuất',7, '009','2020/03/02');
+	print N'thêm dữ liệu thành công'
+END TRY
+
+BEGIN CATCH
+	print N'failure : chèn dữ  liệu không thành công'
+END CATCH
